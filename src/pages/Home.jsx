0149f2954c6d7ef38697d3574e62ee84b9210692
Mihail <vdanilenko254@gmail.com>
2024-09-03 21:53:51 +0300

    full posts created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,24 +9,26 @@ import { useEffect, useState } from "react";
 import axios from "../axios";
 
 export const Home = () => {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [posts, setPosts] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get("/posts");
-        setData(response.data);
+        setPosts(response.data.posts);
       } catch (err) {
         setError("Ошибка при загрузке данных");
       } finally {
-        setLoading(false);
+        setIsLoading(false);
       }
     };
 
     setTimeout(() => fetchData(), 1000);
   }, []);
+
+  console.log(posts);
 
   return (
     <>
@@ -40,25 +42,32 @@ export const Home = () => {
       </Tabs>
       <Grid container spacing={4}>
         <Grid xs={8} item>
-          <Post
-            id="1"
-            title="Roast the code #1 | Rock Paper Scissors"
-            imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
-            user={{
-              avatarUrl:
-                "https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png",
-              fullName: "Keff",
-            }}
-            createdAt={"12 июня 2022 г."}
-            viewsCount={150}
-            commentsCount={3}
-            tags={["one", "two"]}
-            isLoading={loading}
-            isEditing={true}
-          />
+          {isLoading ? (
+            <Post isLoading={isLoading} />
+          ) : (
+            posts.map((item) => (
+              <Post
+                key={item._id}
+                _id={item._id}
+                title={item.title}
+                imageUrl={`http://localhost:8000/uploads/${item.imageUrl}`}
+                user={{
+                  avatarUrl:
+                    "https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png",
+                  fullName: [item["user"].fullName],
+                }}
+                createdAt={item.createdAt.toString().slice(0, 10)}
+                viewsCount={item.viewsCount}
+                commentsCount={3}
+                tags={["one", "two"]}
+                isLoading={false}
+                isEditing={true}
+              />
+            ))
+          )}
         </Grid>
         <Grid xs={4} item>
-          <TagsBlock items={["header", "header"]} isLoading={false} />
+          <TagsBlock items={["header", "header"]} isLoading={isLoading} />
           <CommentsBlock
             items={[
               {
